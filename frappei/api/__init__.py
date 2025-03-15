e21ca3485e07b2889d4682181f44e0bc2b4e5f48
Vishal Kumar <vishnshiv3@gmail.com>
2025-03-15 15:29:42 +0530

    load modules of the given basename

--- frappei/api/__init__.py
+++ frappei/api/__init__.py
@@ -2,25 +2,34 @@ from werkzeug.routing import Rule, Submount, Map
 from werkzeug.wrappers import Request, Response
 from werkzeug.exceptions import NotFound
 from frappe.exceptions import DoesNotExistError
-import frappei
 import frappe
+import frappei
 from frappe.handler import build_response
 from frappe.api import API_URL_MAP
+import importlib
+import pkgutil
+import os
 
 
 def handle_api_call(route: str):
-    # try:
-    route = route.split("/")
-    if len(route) == 2:
-        app_route, api_name = route
-        method = frappei.frappei_whitelisted[app_route]["apis"]["base"][api_name]
-        return frappe.call(method, **frappe.form_dict)
-    if len(route) == 3:
-        app_route, sub_group, api_name = route
-        method = frappei.frappei_whitelisted[app_route]["apis"][sub_group][api_name]
-        return frappe.call(method, **frappe.form_dict)
-    # except Exception:
-    #     raise DoesNotExistError
+        
+    #try:
+    
+        route = route.split("/")
+
+        if len(route) == 2:
+            find_function_in_app(route[0])
+            app_route, api_name = route
+            method = frappei.frappei_whitelisted[app_route]["apis"]["base"][api_name]
+            return frappe.call(method, **frappe.form_dict)
+            
+        if len(route) == 3:
+            find_function_in_app(route[0])
+            app_route, sub_group, api_name = route
+            method = frappei.frappei_whitelisted[app_route]["apis"][sub_group][api_name]
+            return frappe.call(method, **frappe.form_dict)
+        
+        #raise DoesNotExistError
 
 
 url_rules = [
@@ -51,3 +60,26 @@ def handle(request: Request):
         if data is not None:
             frappe.response["message"] = data
         return build_response("json")
+
+def find_function_in_app(app_name):
+    """
+    Recursively searches for a function in any module inside the given app.
+    
+    :param app_name: The root package name (e.g., "frappe")
+    :param function_name: The function name to search for
+    :return: The function object if found, else None
+    """
+
+    try:
+        package = importlib.import_module(app_name)
+    except ModuleNotFoundError:
+        raise DoesNotExistError
+    
+    package = importlib.import_module(app_name)
+    package_path = os.path.dirname(package.__file__)
+
+    for _, module_name, _ in pkgutil.walk_packages([package_path], prefix=f"{app_name}."):
+        try:
+            module = importlib.import_module(module_name)
+        except:
+            pass
